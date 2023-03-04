a107bff7a0697e1c9e475aac5bb64f73c2ec8d37
Q <foldervapp@gmail.com>
2023-03-04 11:24:55 +0800

    androidx

--- app/src/main/java/com/monkey/dragbubbleview/MainActivity.java
+++ app/src/main/java/com/monkey/dragbubbleview/MainActivity.java
@@ -1,10 +1,11 @@
 package com.monkey.dragbubbleview;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.monkey.dragbubbleview.DragBubbleView.OnBubbleStateListener;
 
